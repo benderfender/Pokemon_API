55f8de5ca70fd609485429e598fcdb09c511d15d
benderfender <67402670+benderfender@users.noreply.github.com>
2020-09-10 10:50:55 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Axios from 'axios';
 
 function App() {
+
+  const [facts,setFacts] = useState([]);
+
+  //Axios needs .data for it to work.
+  const getFacts = () => {
+    Axios.get("https://pokeapi.co/api/v2/pokemon")
+      .then(response=>setFacts(response.data.results))
+      .catch(err=>console.log(err))
+  }
+
+  //.results is specific to pokemon url.
+  // const getFacts = () => {
+  //   fetch("https://pokeapi.co/api/v2/pokemon")
+  //     .then(response => response.json())
+  //     .then(response => {
+  //       setFacts(response.results);
+  //     })
+  //     .catch(err => console.log(err))
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <button className="btn btn-primary btn-lg" onClick={getFacts}> Fetch Pokemon </button>
+        { 
+        facts.map((pokemon,index) => {
+            return <p key={index}>{pokemon.name}</p>
+        })
+        }
       </header>
     </div>
   );
